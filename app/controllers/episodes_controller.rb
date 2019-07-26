5902162fab7fd2f19cff2955ee4d76b2f37fe138
TeenageGrampa <danielhvoigt@gmail.com>
2019-07-26 11:06:52 -0400

    Done

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -3,4 +3,53 @@ class EpisodesController < ApplicationController
   def index
     @episodes = Episode.all
   end
+
+  def show
+    find_episode
+  end
+
+  def new 
+    @episode = Episode.new 
+  end
+
+  def create
+    @episode = Episode.create(episode_params)
+    if @episode.valid?
+      redirect_to new_appearance_path
+    else
+      flash[:errors] = @episode.errors.full_messages
+      redirect_to new_episode_path
+    end
+  end
+
+  def edit 
+    find_episode
+  end 
+
+  def update 
+    find_episode
+    @episode.update(episode_params)
+    if @episode.valid?
+      redirect_to episode_path(@episode.id)
+    else
+      flash[:errors] = @episode.errors.full_messages
+      redirect_to edit_episode_path(@episode.id)
+    end
+  end
+
+  def destroy
+    find_episode
+    @episode.destroy
+    redirect_to episodes_path
+  end
+
+  private
+
+  def find_episode 
+    @episode = Episode.find(params[:id])
+  end
+
+  def episode_params 
+    params.require(:episode).permit(:date, :number)
+  end
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,6 @@
-class Guest < ApplicationRecord
+class Guest < ApplicationRecord 
+    has_many :appearances
+    has_many :episodes, through: :appearances
 
+    validates :name, :occupation, presence: true
 end
